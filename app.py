c92a7e424f81fabdd6ee3661326d2cfa7990a237
Nuel <adegunlehinabayomi@yahoo.com>
2020-04-11 12:47:52 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template, flash, request, session, jsonify, redirect, url_for, send_from_directory
+from src.estimator import estimator
+import ast
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/api/v1/on-covid-19', methods = ['POST'])
+def getInputData():
+
+    dat = request.args.get('data')
+    print(type(dat))
+    data = ast.literal_eval(dat)
+    print(type(data))
+
+
+    result = jsonify({'':estimator(data)})   
+
+    return result
+@app.route('/')
+def index():
+    return "<h1> Welcome here </h1>" 
+
+
+
+if __name__=='__main__':
+    app.run(debug=True, host="0.0.0.0")
